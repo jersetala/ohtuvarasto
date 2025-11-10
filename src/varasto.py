82fafca10d0ffa38f5c41fdb3af49e71766dfbc8
jersetala <jeremias.setala@helsinki.fi>
2025-11-10 18:40:33 +0200

    Added specified .pylintrc and broke every single rule

--- src/varasto.py
+++ src/varasto.py
@@ -9,12 +9,13 @@ class Varasto:
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
-        elif alku_saldo <= tilavuus:
-            # mahtuu
-            self.saldo = alku_saldo
         else:
-            # täyteen ja ylimäärä hukkaan!
-            self.saldo = tilavuus
+            if alku_saldo <= tilavuus:
+                # mahtuu
+                self.saldo = alku_saldo
+            else:
+                # täyteen ja ylimäärä hukkaan!
+                self.saldo = tilavuus
 
     # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
